069e5fd9c485c8a0098cbb0ef42d1dd2c13040f7
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-12-13 16:47:47 +0300

    add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,9 +1,18 @@
+import pathlib
+from pathlib import Path
+
 from django.db import models  # noqa F401
+from django.utils import timezone
 
 # your models here
 class Pokemon(models.Model):
     # text = models.TextField()
     title = models.CharField(max_length=200, verbose_name="Название покемона")
+
+    photo = models.ImageField(upload_to='media',
+                              blank=True,
+                              verbose_name="Фото покемона"
+                              )
 
     def __str__(self):
         return self.title
