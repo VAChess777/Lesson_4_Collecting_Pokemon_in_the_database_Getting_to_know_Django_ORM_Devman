a044ee595ee7dc44c869a7f6727eb7d237854b10
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-12-13 18:41:04 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,16 +6,27 @@ from django.utils import timezone
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Pokemon Name')
-    photo = models.ImageField(upload_to='media', blank=True, verbose_name='Pokemon Photo')
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Pokemon Name'
+    )
+    photo = models.ImageField(
+        upload_to='media',
+        blank=True,
+        verbose_name='Pokemon Photo'
+    )
 
     def __str__(self):
         return self.title
 
 
 class PokemonEntity(models.Model):
-    latitude = models.FloatField(verbose_name='Latitude')
-    longitude = models.FloatField(verbose_name='Longitude')
+    latitude = models.FloatField(
+        verbose_name='Latitude'
+    )
+    longitude = models.FloatField(
+        verbose_name='Longitude'
+    )
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE,
@@ -29,4 +40,29 @@ class PokemonEntity(models.Model):
     disappeared_at = models.DateTimeField(
         default=timezone.now,
         verbose_name="Time of disappearance"
+    )
+    level = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Level"
+    )
+    health = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Health"
+        )
+    attack = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Attack"
+    )
+    protection = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Protection"
+    )
+    endurance = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Endurance"
     )
