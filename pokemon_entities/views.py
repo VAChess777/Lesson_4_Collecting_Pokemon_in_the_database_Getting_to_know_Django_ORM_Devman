329d4cc2b1396d68486bca85d8e00ad69f17ce6e
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-12-13 19:33:59 +0300

    show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from pokemon_entities.models import Pokemon
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -31,6 +32,7 @@ def show_all_pokemons(request):
         pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+
     for pokemon in pokemons:
         for pokemon_entity in pokemon['entities']:
             add_pokemon(
@@ -40,12 +42,16 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
-        })
+
+    for pokemon in Pokemon.objects.all():
+        pokemons_on_page.append(
+            {
+            'pokemon_id': pokemon.id,
+            'img_url': request.build_absolute_uri(pokemon.photo.url),
+            'title_ru': pokemon.title,
+            }
+        )
+
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
